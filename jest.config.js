7fd5e3bcbf23cbd9c38c4d724ded13df86c6888c
Kirk Swenson <kswenson@concord.org>
2024-01-09 21:01:13 -0800

    chore: package.json changes for publication

--- jest.config.js
+++ jest.config.js
@@ -12,5 +12,9 @@ module.exports = {
   ],
   "transform": {
     "^.+\\.(js|jsx|ts|tsx)$": "ts-jest"
-  }
+  },
+  "transformIgnorePatterns": [
+    "/comments/ESM-only (https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c) modules that should not be transformed by ts-jest",
+    "/node_modules/(?!(mime)/)"
+  ],
 }
