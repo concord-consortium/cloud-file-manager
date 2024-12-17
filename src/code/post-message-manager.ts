4dab05a8ced5e793756e8d891294c251acf71c27
Paul Kim / 김바울 <paulkim3151@gmail.com>
2024-12-17 17:23:46 +0900

    Revert "Revert "TASK-2250 코답 OF (CFM)""

--- src/code/post-message-manager.ts
+++ src/code/post-message-manager.ts
@@ -16,3 +16,42 @@ export function registerSavePostMessage(
     },
   })
 }
+export function registerReloadPostMessage(
+  reload: () => Promise<string | null>
+) {
+  postMessageManager.register({
+    messageType: "reload",
+    callback: async () => {
+      return await reload()
+    },
+  })
+}
+
+export function registerLoadProjectPostMessage(
+  loadProject: (projectId: string) => Promise<string | null>
+) {
+  postMessageManager.register({
+    messageType: "loadProject",
+    callback: async (projectId: string) => {
+      return await loadProject(projectId)
+    },
+  })
+}
+
+export function postiframeLoadedMessageToParent() {
+  if (window === window.parent) return
+  const iframeKey = new URLSearchParams(window.location.search).get(
+    "iframe_key"
+  )
+  if (!iframeKey) return
+  try {
+    postMessageManager.send({
+      messageType: `iframeLoaded-${iframeKey}`,
+      payload: null,
+      target: window.parent,
+      targetOrigin: "*",
+    })
+  } catch {
+    /* ignore */
+  }
+}
